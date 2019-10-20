1dccac2a373d2c715afd75aa70b82919789989c3
Felipe Ferreira Cabo <fcabo@esapiens.com.br>
2019-10-20 09:42:45 -0300

    cli test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { useState ,useEffect} from 'react';
 import axios from 'axios'; 
+
 function App() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-      axios.get( 'http://localhost:3030/search/gta')
+    setPosts('');
+      axios.get( 'http://localhost:3030/search/linux')
     .then(response => {
       console.log(response.data);
       setPosts(response.data);
@@ -15,7 +17,7 @@ function App() {
     });
   }, []);
 
-    
+  if (posts){
   return (
     <ul>
       {posts.map((item,index) => (
@@ -25,5 +27,10 @@ function App() {
       ))}
     </ul>
   );
+}else{
+  return (
+ <p>carregando...</p>
+  )
+}
 }
 export default App;
