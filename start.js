0ebf6e80602914f83a31b0159a148bc7904bac09
Felipe Ferreira Cabo <felipeferreirah@gmail.com>
2019-10-20 10:23:04 -0300

    Organization start.js and commands from package.json

--- start.js
+++ start.js
@@ -1,11 +1,20 @@
-const {app, BrowserWindow} = require('electron')     
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
-function createWindow () {   
-    // Create the browser window.     
-  let win = new BrowserWindow({width: 800, height: 600}) 
-         
-  // and load the index.html of the app.      
-  win.loadURL('http://localhost:3000/')
-}     
-   app.on('ready', createWindow)
+let mainWindow;
 
+function createWindow() {  
+  mainWindow = new BrowserWindow({width: 900, height: 680});  
+  mainWindow.loadURL('http://localhost:3000/');
+  
+  app.setAboutPanelOptions({    
+    applicationName: 'Exogames',    
+    applicationVersion: '0.0.1',  
+  });
+
+  mainWindow.on('closed', () => mainWindow = null);
+}
+  app.on('ready', createWindow);
+  app.on('window-all-closed', () => {  if (process.platform !== 'darwin') {    app.quit();  }});
+  app.on('activate', () => {  if (mainWindow === null) {    createWindow();  }});
