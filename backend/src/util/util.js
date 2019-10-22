b683a0521c7400be137f2c02f6f0653035e025c1
Leonardo Paleari <leologe@gmail.com>
2019-10-22 00:01:49 -0300

    Fixed when there is no value on api return

--- backend/src/util/util.js
+++ backend/src/util/util.js
@@ -1,9 +1,12 @@
 const {format} = require('date-fns');
 
 exports.convertDate = function (date) {
-  let newDate = new Date(date.replace('th','').replace('\'','').replace('rd','').replace('st','').replace('nd',''));
+  try{
+    let newDate = new Date(date.replace('th','').replace('\'','').replace('rd','').replace('st','').replace('nd',''));
   
+    return format(newDate, 'dd/MM/yyyy');
 
-
-  return format(newDate, 'dd/MM/yyyy');
+  } catch (err) {
+    return null;
+  }
 }
