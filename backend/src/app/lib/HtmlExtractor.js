418aba04a947fda236ea5bd26f0aef1e737f8606
Leonardo Paleari <leologe@gmail.com>
2019-10-22 00:01:01 -0300

    created HtmlExtractor library

---
+++ backend/src/app/lib/HtmlExtractor.js
@@ -0,0 +1,31 @@
+const { parse } = require('node-html-parser');
+const fs = require('fs')
+
+module.exports = function extractData(torrentHTML) {
+    var data = {}
+    const root = parse(torrentHTML);
+    /**
+     * Get images
+     */
+    let img = root.querySelectorAll("img")
+    try{
+        data.cover = img[0].rawAttributes['data-original'];
+
+        data.print = [];
+        img.forEach(element => {
+            data.print.push(element.rawAttributes['data-original']);
+        });
+        data.print.shift();
+    } catch (err){
+        data.print = null
+    }
+    /**
+     * Get description
+     */
+    let description = root.querySelector('#description');
+    data.desc = description.rawText;
+
+
+    return data
+}
+
