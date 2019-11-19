4fc568c9c4c9e0a7f5633a499fcbb4b5daad322d
Felipe Ferreira Cabo <fcabo@esapiens.com.br>
2019-11-18 21:11:32 -0300

    update new frontend structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app/pages/home/home';
-import './app/assets/less/app.less';
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import './styles/theme.scss'
+import App from './app'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#root')
+)
