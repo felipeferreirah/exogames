50198ea7856ee46cc8c7b726e4e96a5ea0713f93
felipe <felipeferreirah@gmail.com>
2022-02-01 19:27:03 -0300

    alguns ajustes

--- backend/app/helpers/HtmlExtractor.js
+++ backend/app/helpers/HtmlExtractor.js
@@ -1,30 +1,30 @@
-const { parse } = require('node-html-parser');
-const { urlExist } = require('url-exist');
+  const { parse } = require('node-html-parser');
+  const { urlExist } = require('url-exist');
 
-module.exports = function extractData(torrentHTML) {
-    var data = {};
-    const root = parse(torrentHTML);
-    /**
-     * Get images
-     */
-    let img = root.querySelectorAll("img");
-    try {
-        data.cover = img[0].rawAttributes['data-original'];
-        data.print = [];
-        img.forEach(element => {
-            data.print.push(element.rawAttributes['data-original']);
-        });
-        data.print.shift();
-    } catch (err) {
-        data.print = null;
-    }
-    /**
-     * Get description
-     */
-    let description = root.querySelector('#description');
-    data.desc = description.rawText;
+  module.exports = function extractData(torrentHTML) {
+      var data = {};
+      const root = parse(torrentHTML);
+      /**
+       * Get images
+       */
+      let img = root.querySelectorAll("img");
+      try {
+          data.cover = img[0].rawAttributes['data-original'];
+          data.print = [];
+          img.forEach(element => {
+              data.print.push(element.rawAttributes['data-original']);
+          });
+          data.print.shift();
+      } catch (err) {
+          data.print = null;
+      }
+      /**
+       * Get description
+       */
+      let description = root.querySelector('#description');
+      data.desc = description ? description.rawText : null
 
 
-    return data;
-};
+      return data;
+  };
 
